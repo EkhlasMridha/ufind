dcea0eb47276c2fa274ddde7c0d37e9a9e533567
EkhlasMridha <41969194+EkhlasMridha@users.noreply.github.com>
2021-02-02 21:20:19 +0600

    user auth serializers created

--- ufind/settings.py
+++ ufind/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'user',
+    'ufindAPI',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- user/serializers.py
+++ user/serializers.py
@@ -2,7 +2,27 @@ from rest_framework import serializers
 from user.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserProfile(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'name']
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'name', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def save(self):
+        user = User(
+            email=self.validated_data['email'],
+            name=self.validated_data['name']
+        )
+        password = self.validated_data['password']
+        user.set_password(password)
+        user.save()
+
+        return user
