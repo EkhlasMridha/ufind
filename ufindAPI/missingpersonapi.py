f93b51a1f89320472dde7f8fdad89f2e4051939f
EkhlasMridha <41969194+EkhlasMridha@users.noreply.github.com>
2021-02-15 14:11:03 +0600

    serve image file

--- ufindAPI/missingpersonapi.py
+++ ufindAPI/missingpersonapi.py
@@ -6,10 +6,12 @@ from rest_framework.response import Response
 from django.conf import settings
 
 from missing.serializers import MissingPersonSerializer
+from missing.models import MissingPerson
 
 
 @api_view(['POST'])
 @parser_classes([MultiPartParser, FormParser])
+@permission_classes([IsAuthenticated])
 def submit_case_view(request):
     serializers = MissingPersonSerializer(data=request.data)
     if serializers.is_valid():
@@ -17,3 +19,11 @@ def submit_case_view(request):
         return Response("Created", status=status.HTTP_200_OK)
     else:
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def get_cases_view(request):
+    result = MissingPerson.objects.all()
+    serialized_result = MissingPersonSerializer(result, many=True)
+
+    return Response(serialized_result.data, status=status.HTTP_200_OK)
--- ufindAPI/urls.py
+++ ufindAPI/urls.py
@@ -2,16 +2,18 @@ from django.urls import path, include
 from ufindAPI.identityapi import (
     login_api_view,
     register_api_view,
-    get_profile_api
+    get_profile_api,
 )
 
 from ufindAPI.missingpersonapi import (
-    submit_case_view
+    submit_case_view,
+    get_cases_view
 )
 
 urlpatterns = [
     path('login', login_api_view, name="login"),
     path('register', register_api_view, name='register'),
     path('profile', get_profile_api, name="profile"),
-    path('case', submit_case_view, name="case")
+    path('case', submit_case_view, name="case"),
+    path('get-cases', get_cases_view, name='get-cases')
 ]
